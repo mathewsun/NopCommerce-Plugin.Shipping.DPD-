2b98a6dab1e39a0b0d6b5328b99577724e96fbcd
AlexPigalyov <alex.pigalyov@gmail.com>
2021-02-01 12:51:55 +0300

    initial commit + Stable version 1.0.0

---
+++ Infrastructure/PluginDefaults.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nop.Plugin.Shipping.DPD.Infrastructure
+{
+    public static class PluginDefaults
+    {
+        public static string PluginBasePath => "~/Plugins/Shipping.DPD/";
+        public static string CustomCheckoutViewPathFormat => $"{PluginBasePath}Views/Checkout/";
+        public static string CustomSharedViewPathFormat = $"{PluginBasePath}Views/Shared/";
+        public static string ContentPath = $"{PluginBasePath}Content/";
+        public static string ScriptsPath => $"{PluginBasePath}Content/scripts/";
+        public static string StylesPath => $"{PluginBasePath}Content/Styles/";
+        
+    }
+}
---
+++ Models/DPDCheckoutShippingMethodModel.cs
@@ -0,0 +1,13 @@
+﻿using Nop.Web.Models.Checkout;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nop.Plugin.Shipping.DPD.Models
+{
+    public class DPDCheckoutShippingMethodModel : CheckoutShippingMethodModel
+    {
+        public OnePageCheckoutModel OnePageModel { get; set; }
+    }
+}
